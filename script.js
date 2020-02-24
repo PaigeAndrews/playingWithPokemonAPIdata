4d6fe7d9cec79815deec9a9a9c2201ab8bdd6218
Paige Andrews <pandrews25@yahoo.com>
2020-02-24 15:34:24 -0500

    added some more api data to page for each pokemon and event listeners

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let button = document.getElementsByClassName("myButton")[0];
+const input = document.querySelector('#myText');
+
 
 button.addEventListener("click", myFunction);
+input.addEventListener('keyup',function(e){
+  if (e.keyCode === 13) {
+  myFunction();
+}
+});
 
 const request = async (choice) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${choice}/`);
@@ -10,9 +17,19 @@ const request = async (choice) => {
 }
 
 
-function myFunction() {
-  let choice = document.getElementById("myText").value;
-  request(choice)
-  document.getElementById("demo").innerHTML = choice;
-console.log(choice)
+async function myFunction() {
+  let choice = document.getElementById("myText").value.toLowerCase();
+  let result = await request(choice)
+  
+  console.log(choice)
+  // abilities = result.abilities
+  document.getElementById("sprites").innerHTML.src = 
+  // result.sprites.front_default
+  document.getElementById("pokeName").innerHTML = choice;
+  // document.getElementById("pokeAbilities").innerHTML = result.abilities.map(element => " " + element.ability.name )
+  document.getElementById("pokeAbilities").innerHTML = result.moves.map(element => " " + element.move.name )
+  document.getElementById("pokeId").innerHTML = "# " + result.id 
+  document.getElementById("pokeType").innerHTML = result.types[0].type.name
+  document.getElementById("pokeStat").innerHTML = result.stats.map(element => " " + element.stat.name)
+  document.getElementById("pokeItems").innerHTML = result.held_items.map(element => " " + element.item.name + " - rarity: " + element.version_details[0].rarity)
 }
